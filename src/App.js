f697dece9c926fa28cf935968edbd8f037de4516
Isaac <hawkins.isaac@gmail.com>
2019-08-12 16:33:29 -0500

    move pill into app component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import logo from './assets/clear_head.png'
+
 
 import Mortgage from './Components/Mortgage';
+import Greeting from './Components/Mortgage/Greeting';
+import Pill from './Components/Mortgage/Pill';
 
 class App extends Component {
   constructor() {
@@ -21,6 +25,20 @@ class App extends Component {
   render() {
     return (
       <div className="app">
+        <Greeting 
+          message='Greetings, Clearhead team!'
+          img_src={logo}
+        />
+        <section className='pill_container'>
+          <Pill 
+            className='mortgage_pill active'
+            label='Mortgage Calculations'
+          />
+          <Pill 
+            className='affortability_pill' 
+            label='Affortability Calculations'
+          />
+        </section>
         <Mortgage getMorgage={this.setValue}/>
       </div>
     );
--- src/Components/Mortgage/index.js
+++ src/Components/Mortgage/index.js
@@ -1,12 +1,8 @@
 import React, { Component } from 'react';
 import './index.css';
-import logo from '../../assets/clear_head.png'
-
 
 import Calculations from './Calculations';
 import Display from './Display';
-import Greeting from './Greeting';
-import Pill from './Pill';
 
 
 class Mortgage extends Component {
@@ -25,20 +21,6 @@ class Mortgage extends Component {
   render() {
     return (
       <main className='calculator'>
-        <Greeting 
-          message='Greetings, Clearhead team!'
-          img_src={logo}
-        />
-        <section className='pill_container'>
-          <Pill 
-            className='mortgage_pill active'
-            label='Mortgage Calculations'
-          />
-          <Pill 
-            className='affortability_pill' 
-            label='Affortability Calculations'
-          />
-        </section>
         <Calculations getPayment={this.getPayment}/>
         <Display payment={this.state.monthly_payment} />
       </main>
